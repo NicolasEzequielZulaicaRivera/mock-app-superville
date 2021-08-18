f6a617fc3a0790fe45b2357006b85ab33dba9e00
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-08-18 13:58:21 -0300

    stepper ini

--- src/main/components/Emitir/Emitir.jsx
+++ src/main/components/Emitir/Emitir.jsx
@@ -1,4 +1,4 @@
-import { Button } from '@material-ui/core';
+import { Button, Step, StepLabel, Stepper } from '@material-ui/core';
 import React from 'react';
 import {connect} from 'react-redux';
 import './Emitir.scss';
@@ -9,21 +9,36 @@ import './Emitir.scss';
  * @return { * } component
  */
 const Emitir = (props) => {
-    const {
-
-    } = props;
+    const [activeStep, setActiveStep] = React.useState(0);
+    const steps = ['Confirmar Datos','Direccion','Medio de Pago'];
 
     return (
         <div className="c-container">
-            <div className="title"><h3>Cotizacion creada</h3></div>
+            <div className="title"><h3>Emitir</h3></div>
 
             <div className="main-container">
             
                 <div className="data-card w-container">
-                    HOLA
+                        <h4>Plan F</h4>
+                        <h5>cuota mensual</h5>
+                        <h2>$1.726</h2>
+                        <br />
+                        <h5>mosto asegurado</h5>
+                        <h3>$81.300</h3>
+                        <div className="controls" ><Button className="secondary-button" href="/cotizar/resultados">Cambiar</Button></div>
                 </div>
                 <div className="main-card w-container">
-                    HOLA
+                    
+                <Stepper activeStep={activeStep} alternativeLabel>
+                    {steps.map((label, index) => {
+                    return (
+                        <Step key={label} >
+                            <StepLabel >{label}</StepLabel>
+                        </Step>
+                    );
+                    })}
+                </Stepper>
+
                 </div>
 
             </div>
