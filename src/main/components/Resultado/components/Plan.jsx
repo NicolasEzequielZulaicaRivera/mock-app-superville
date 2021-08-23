debb7cce97c2e2bc1042c9ebabd527336a7e53c9
AndreaOpazo <andree.2593@gmail.com>
2021-08-23 09:58:28 -0300

    modifico vista resultado

---
+++ src/main/components/Resultado/components/Plan.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+const plans = [
+  {
+    name: 'Plan E',
+    cuotaMensual: 1153,
+    sumaAsegurada: 54300,
+    coberturaAnioVigencia: '2 eventos al 100%',
+    antiguedadEquipo: 'sin límite',
+    ambitoCobertura: 'Argentina',
+    asistencias: {
+      emergenciasMedicas: '-',
+      cerrajeria: '✅',
+      taxiRemis: '✅'
+    }
+  },
+  {
+    name: 'Plan F',
+    cuotaMensual: 1726,
+    sumaAsegurada: 81300,
+    coberturaAnioVigencia: '2 eventos al 100%',
+    antiguedadEquipo: 'sin límite',
+    ambitoCobertura: 'Argentina',
+    asistencias: {
+      emergenciasMedicas: '-',
+      cerrajeria: '✅',
+      taxiRemis: '✅'
+    }
+  },
+  {
+    name: 'Plan G',
+    cuotaMensual: 2304,
+    sumaAsegurada: 108500,
+    coberturaAnioVigencia: '2 eventos al 100%',
+    antiguedadEquipo: 'sin límite',
+    ambitoCobertura: 'Argentina y países limítrofes',
+    asistencias: {
+      emergenciasMedicas: '✅',
+      cerrajeria: '✅',
+      taxiRemis: '✅'
+    }
+  }
+];
+
+const Plan = () => {
+  return (
+    <>
+    {
+      plans.map(plan => (
+        <div className="w-container sub-card">
+          <div className="card-header">
+            <h3>{plan.name}</h3>
+            <h5>cuota mensual</h5>
+            <h2>${plan.cuotaMensual}</h2>
+            <div className="controls">
+              <Button className="primary-button" href="/cotizar/emitir">Emitir</Button>
+            </div>
+          </div>
+          <div className="c-row"><h3>${plan.sumaAsegurada}</h3></div>
+          <div className="c-row"><h4>{plan.coberturaAnioVigencia}</h4></div>
+          <div className="c-row"><h4>{plan.antiguedadEquipo}</h4></div>
+          <div className="c-row"><h4>{plan.ambitoCobertura}</h4></div>
+          <div className="c-row"></div>
+          <div className="c-row"><h4>{plan.asistencias.emergenciasMedicas}</h4></div>
+          <div className="c-row"><h4>{plan.asistencias.cerrajeria}</h4></div>
+          <div className="c-row"><h4>{plan.asistencias.taxiRemis}</h4></div>
+        </div>
+      ))
+    }
+    </>
+  )
+};
+
+export default Plan;
