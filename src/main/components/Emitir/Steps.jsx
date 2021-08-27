80f32cc649aa65b7f44ad20ffef623f6ec66520d
AndreaOpazo <andree.2593@gmail.com>
2021-08-27 15:56:36 -0300

    arreglo checkbox step 2

--- src/main/components/Emitir/Steps.jsx
+++ src/main/components/Emitir/Steps.jsx
@@ -1,31 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import cotizacionesActions from 'cotizaciones/cotizaciones.actions';
+import React, { useEffect, useRef } from "react";
 
-
-function Step1(props){
-
-    
-    const [ state , setState ] = useState({
-        name: "",
-        surname: "",
-        documentType: "",
-        documentNumber: "",
-        idType: "",
-        idNumber: "",
-        email: "",
-        areaCode:"",
-        phone:"",
-        ...props
-    })
-
-    const handleInputChange = (event)=>{
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        setState( {...state, [name]: value } )
-    }
+const Step1 = (props) => {
     
     return (
 		<div className="form">
@@ -35,14 +10,14 @@ function Step1(props){
 					<div className="f-label"><label >Nombre</label></div>
 					<div className="f-row">
 						<div className="f-row">
-							<input type="text" placeholder="" name="name" value={state.name} onChange={handleInputChange}/>
+							<input type="text" placeholder="" name="name" defaultValue={props.name}/>
 						</div>
 					</div>
 				</div>
 				<div className="f-col">
 					<div className="f-label"><label >Apellido</label></div>
 					<div className="f-row">
-						<input type="text" placeholder="" name="surname" value={state.surname} onChange={handleInputChange}/>
+						<input type="text" placeholder="" name="surname" defaultValue={props.surname}/>
 					</div>
 				</div>
 			</div>
@@ -50,35 +25,35 @@ function Step1(props){
 				<div className="f-col">
 					<div className="f-label"><label >Documento de identidad</label></div>
 					<div className="f-row">
-						<select name="documentType" value={state.documentType} onChange={handleInputChange}>
+						<select name="documentType" defaultValue={props.documentType}>
 							<option value="">Tipo</option>
 							<option value="DNI">DNI</option>
 						</select>
-						<input type="text" placeholder="Numero" name="documentNumber" value={state.documentNumber} onChange={handleInputChange}/>
+						<input type="text" placeholder="Numero" name="documentNumber" defaultValue={props.documentNumber}/>
 					</div>
 				</div>
 				<div className="f-col">
 					<div className="f-label"><label >Codigo de identificacion</label></div>
 					<div className="f-row">
-						<select name="idType" value={state.idType} onChange={handleInputChange}>
+						<select name="idType" defaultValue={props.idType}>
 							<option value="">Tipo</option>
 							<option value="1">COD</option>
 						</select>
-						<input type="text" placeholder="Numero" name="idNumber" value={state.idNumber} onChange={handleInputChange}/>
+						<input type="text" placeholder="Numero" name="idNumber" defaultValue={props.idNumber}/>
 					</div>
 				</div>
 			</div>
 			<div className="f-label"><label >Correo electronico</label></div>
 			<div className="f-row">
-				<input type="text" placeholder="" name="email" value={state.email} onChange={handleInputChange}/>
+				<input type="text" placeholder="" name="email" defaultValue={props.email}/>
 			</div>
 			<div className="f-label"><label >Telefono</label></div>
 			<div className="f-cols">
 				<div className="f-col w30">
-					<div className="f-row"><input type="text" placeholder="Cod. area" name="areaCode" value={state.areaCode} onChange={handleInputChange}/></div>
+					<div className="f-row"><input type="text" placeholder="Cod. area" name="areaCode" defaultValue={props.areaCode}/></div>
 				</div>
 				<div className="f-col">
-					<div className="f-row"><input type="number" placeholder="Numero" name="phone" value={state.phone} onChange={handleInputChange}/></div>
+					<div className="f-row"><input type="number" placeholder="Numero" name="phone" defaultValue={props.phone}/></div>
 				</div>
 			</div>
 			
@@ -86,34 +61,29 @@ function Step1(props){
 	)
 }
 
-function Step2(props){
+const Step2 = (props) => {
 
-    const [ state , setState ] = useState({
-        riskDirection: {province: props.province,city: "",street: "",numstreet: "",floor: "",dept: "",postalCode: "",},
-        sameDir: true,
-        clientDirection: {province: props.province,city: "",street: "",numstreet: "",floor: "",dept: "",postalCode: "",},
-        ...props,
-    })
+    props = { sameDir: true, ...props }
 
-    const handleInputChange = (event)=>{
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
+    const clientDirection = useRef(null);
+    const sameDir = useRef(null);
 
-        setState( {...state, [name]: value } )
+    const handleChange = (e) => {
+        sameDir.current.value = e.target.checked ? '1' : '';
+        if (e.target.checked) {
+            clientDirection.current.classList.add('hidden');
+        } else {
+            clientDirection.current.classList.remove('hidden');
+        }
     }
-    const dirChange = (event)=>{
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name.split(".");
 
-        const dir = name[0];
-        const prop = name[1];
-
-        const newDir = { ...state[dir] , [prop]:value }
-
-        setState( {...state, [dir]: newDir } )
-    }
+    useEffect(() => {
+        if (props.sameDir) {
+            clientDirection.current.classList.add('hidden');
+        } else {
+            clientDirection.current.classList.remove('hidden');
+        }
+    }, [])
 
 	return (
 		<div className="form">
@@ -123,7 +93,7 @@ function Step2(props){
 				<div className="f-col">
 					<div className="f-label"><label >Provincia</label></div>
 					<div className="f-row">
-						<select name="riskDirection.province" value={state.riskDirection.province} onChange={dirChange}>
+						<select name="riskDirection.province" defaultValue={props.riskDirection?.province}>
 							<option value="BsaAs">Buenos Aires</option>
 							<option value="Mendoza">Mendoza</option>
 						</select>
@@ -132,7 +102,7 @@ function Step2(props){
 				<div className="f-col">
 					<div className="f-label"><label >Ciudad</label></div>
 					<div className="f-row">
-						<input type="text" placeholder="" name="riskDirection.city" value={state.riskDirection.city} onChange={dirChange}/>
+						<input type="text" placeholder="" name="riskDirection.city" defaultValue={props.riskDirection?.city}/>
 					</div>
 				</div>
 			</div>
@@ -140,13 +110,13 @@ function Step2(props){
 				<div className="f-col">
 					<div className="f-label"><label >Calle</label></div>
 					<div className="f-row">
-						<input type="text" placeholder="" name="riskDirection.street" value={state.riskDirection.street} onChange={dirChange}/>
+						<input type="text" placeholder="" name="riskDirection.street" defaultValue={props.riskDirection?.street}/>
 					</div>
 				</div>
 				<div className="f-col w40">
 					<div className="f-label"><label >Numero</label></div>
 					<div className="f-row">
-						<input type="number" placeholder="Numero" name="riskDirection.numstreet" value={state.riskDirection.numstreet} onChange={dirChange}/>
+						<input type="number" placeholder="Numero" name="riskDirection.numstreet" defaultValue={props.riskDirection?.numstreet}/>
 					</div>
 				</div>
 			</div>
@@ -155,43 +125,49 @@ function Step2(props){
 					<div className="f-col">
 						<div className="f-label"><label >Piso</label></div>
 						<div className="f-row">
-							<input type="text" placeholder="" name="riskDirection.floor" value={state.riskDirection.floor} onChange={dirChange}/>
+							<input type="text" placeholder="" name="riskDirection.floor" defaultValue={props.riskDirection?.floor}/>
 						</div>
 						<span className="anotation"> Solo si corresponde </span>
 					</div>
 					<div className="f-col">
 						<div className="f-label"><label >Depto</label></div>
 						<div className="f-row">
-							<input type="text" placeholder="" name="riskDirection.dept" value={state.riskDirection.dept} onChange={dirChange}/>
+							<input type="text" placeholder="" name="riskDirection.dept" defaultValue={props.riskDirection?.dept}/>
 						</div>
 					</div>
 				</div>		
 				<div className="f-col">
 					<div className="f-label"><label >Codigo postal(opcional)</label></div>
 					<div className="f-row">
-						<input type="text" placeholder="" name="riskDirection.postalCode" value={state.riskDirection.postalCode} onChange={dirChange}/>
+						<input type="text" placeholder="" name="riskDirection.postalCode" defaultValue={props.riskDirection?.postalCode}/>
 					</div>
 				</div>
 			</div>
 
 			<div className="f-check-row">
-				<input type="checkbox" className="checkbox" 
-                name="sameDir" onChange={handleInputChange}
-                checked={state.sameDir} 
-            />
+				<input 
+                    type="checkbox"
+                    className="checkbox" 
+                    defaultChecked={props.sameDir} 
+                    onChange={handleChange}
+                />
+                <input 
+                    type="hidden"
+                    name="sameDir"
+                    defaultValue={props.sameDir} 
+                    ref={sameDir}
+                />
 				Utiliza la misma direccion para el cliente
 			</div>
 
-            { state.sameDir?"":(
-            
-            <div className="m-top">
+            <div className="m-top"  ref={clientDirection}>
                 
                 <h5>Direccion del cliente</h5>
                 <div className="f-cols">
 				<div className="f-col">
 					<div className="f-label"><label >Provincia</label></div>
 					<div className="f-row">
-						<select name="clientDirection.province" value={state.clientDirection.province} onChange={dirChange}>
+						<select name="clientDirection.province" defaultValue={props.clientDirection?.province}>
 							<option value="BsaAs">Buenos Aires</option>
 							<option value="Mendoza">Mendoza</option>
 						</select>
@@ -200,7 +176,7 @@ function Step2(props){
 				<div className="f-col">
 					<div className="f-label"><label >Ciudad</label></div>
 					<div className="f-row">
-						<input type="text" placeholder="" name="clientDirection.city" value={state.clientDirection.city} onChange={dirChange}/>
+						<input type="text" placeholder="" name="clientDirection.city" defaultValue={props.clientDirection?.city}/>
 					</div>
 				</div>
 			</div>
@@ -208,13 +184,13 @@ function Step2(props){
                     <div className="f-col">
                         <div className="f-label"><label >Calle</label></div>
                         <div className="f-row">
-                            <input type="text" placeholder="" name="clientDirection.street" value={state.clientDirection.street} onChange={dirChange}/>
+                            <input type="text" placeholder="" name="clientDirection.street" defaultValue={props.clientDirection?.street}/>
                         </div>
                     </div>
                     <div className="f-col w40">
                         <div className="f-label"><label >Numero</label></div>
                         <div className="f-row">
-                            <input type="number" placeholder="Numero" name="clientDirection.numstreet" value={state.clientDirection.numstreet} onChange={dirChange}/>
+                            <input type="number" placeholder="Numero" name="clientDirection.numstreet" defaultValue={props.clientDirection?.numstreet}/>
                         </div>
                     </div>
                 </div>
@@ -223,52 +199,32 @@ function Step2(props){
                         <div className="f-col">
                             <div className="f-label"><label >Piso</label></div>
                             <div className="f-row">
-                                <input type="text" placeholder="" name="clientDirection.floor" value={state.clientDirection.floor} onChange={dirChange}/>
+                                <input type="text" placeholder="" name="clientDirection.floor" defaultValue={props.clientDirection?.floor}/>
                             </div>
                             <span className="anotation"> Solo si corresponde </span>
                         </div>
                         <div className="f-col">
                             <div className="f-label"><label >Depto</label></div>
                             <div className="f-row">
-                                <input type="text" placeholder="" name="clientDirection.dept" value={state.clientDirection.dept} onChange={dirChange}/>
+                                <input type="text" placeholder="" name="clientDirection.dept" defaultValue={props.clientDirection?.dept}/>
                             </div>
                         </div>
                     </div>		
                     <div className="f-col">
                         <div className="f-label"><label >Codigo postal(opcional)</label></div>
                         <div className="f-row">
-                            <input type="text" placeholder="" name="clientDirection.postalCode" value={state.clientDirection.postalCode} onChange={dirChange}/>
+                            <input type="text" placeholder="" name="clientDirection.postalCode" defaultValue={props.clientDirection?.postalCode}/>
                         </div>
                     </div>
                 </div>
 
             </div>
 
-            )}
-
 		</div>
 	)
 }
 
-function Step3(props){
-
-    const [ state , setState ] = useState({
-        cardType: "Debito",
-        creditCard: "",
-        bank: "",
-        cbu: "",
-        cardNumber: "",
-        cardExpiration: "",
-        ...props,
-    })
-
-    const handleInputChange = (event)=>{
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
-
-        setState( {...state, [name]: value } )
-    }
+const Step3 = (props) => {
 
 	return (
 		<div className="form">
@@ -277,18 +233,18 @@ function Step3(props){
                 <div className="f-col w50">
                     <div className="f-label"><label> Tipo </label></div>
                     <div className="f-row">
-                        <select name="cardType" value={state.cardType} onChange={handleInputChange}>
+                        <select name="cardType" defaultValue={props.cardType}>
                             <option value="Debito" >Debito</option>
                             <option value="Credito" >Credito</option>
                         </select>
                     </div>
                 </div>
-                { state.cardType === "Credito"?
+                { props.cardType === "Credito"?
                 (
                 <div className="f-col w50">
                     <div className="f-label"><label> Tarjeta </label></div>
                     <div className="f-row">
-                        <select name="creditCard" value={state.creditCard} onChange={handleInputChange} >
+                        <select name="creditCard" defaultValue={props.creditCard} >
                             <option value="" >Seleccionar tipo</option>
                             <option value="Credito" >Credito</option>
                         </select>
@@ -298,24 +254,24 @@ function Step3(props){
                 
             </div>
             
-            { state.cardType === "Credito"?
+            { props.cardType === "Credito"?
             ( <div>
                 <div className="f-label"><label> Numero </label></div>
 
                 <div className="f-row">
-                    <input type="text" placeholder="" name="cardNumber" value={state.cardNumber} onChange={handleInputChange} />
+                    <input type="text" placeholder="" name="cardNumber" defaultValue={props.cardNumber} />
                 </div>
 
                 <div className="f-label"><label> Vencimiento </label></div>
                 <div className="f-row w30">
-                    <input type="text" placeholder="MM/AA"  name="cardExpiration" value={state.cardExpiration} onChange={handleInputChange}/>
+                    <input type="text" placeholder="MM/AA"  name="cardExpiration" defaultValue={props.cardExpiration}/>
                 </div>
             </div> ):
             ( <div>
                 <div className="f-label"><label> Banco </label></div>
 
                 <div className="f-row">
-                    <select name="bank" value={state.bank} onChange={handleInputChange}>
+                    <select name="bank" defaultValue={props.bank}>
                         <option value="">Seleccionar banco</option>
                         <option value="BBVA">BBVA</option>
                     </select>
@@ -323,7 +279,7 @@ function Step3(props){
 
                 <div className="f-label"><label> CBU </label></div>
                 <div className="f-row">
-                    <input type="text" placeholder="" name="cbu" value={state.cbu} onChange={handleInputChange}/>
+                    <input type="text" placeholder="" name="cbu" defaultValue={props.cbu}/>
                 </div>
             </div> )
             }
@@ -333,9 +289,8 @@ function Step3(props){
 }
 
 
-export function GetStep( {i,...props} ) {
+export const GetStep = ( {i,...props} ) => {
 
-    // Cambie el switch para ver si era la razon por la cual no funcionaba
     switch( i ){
         case 0: return <Step1 {...props} />
         case 1: return <Step2 {...props} />
